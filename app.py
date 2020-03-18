b7e8d0ab3bb0a8ce1d9aab5499ca87ce004e70d3
Waleed BinSaad <wbinsaad@stu.kau.edu.sa>
2020-03-18 10:27:22 +0300

    changing bootStrap and add footer

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ def hello():
 @app.route('/about')
 def about():
     return render_template('about.html')
-    
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
