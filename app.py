9462ba53fcb74b5e7e3bed1e4b3a6e6860cb20e1
Waleed BinSaad <wbinsaad@stu.kau.edu.sa>
2020-03-19 05:56:41 +0300

    singup page not complet need to fix

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, url_for, session, logging, request
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +15,22 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+class SingUpForm(Form):
+    name = StringField('Name', [validators.DataRequired(), validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.email, validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/singup', methods=['get','post'])
+def SingUp():
+    form = SingUpForm(request.form)
+    return render_template('singup.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
