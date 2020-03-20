61beac33a818095544eae684060d0b934a236741
Waleed BinSaad <wbinsaad@stu.kau.edu.sa>
2020-03-20 07:12:47 +0300

    enhance HTML code and make it shorter by formHelper

--- app.py
+++ app.py
@@ -2,18 +2,14 @@ from flask import Flask, render_template, flash, url_for, session, logging, requ
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from flask_wtf import RecaptchaField, Recaptcha
 from passlib.hash import sha256_crypt
-import os
 
 app = Flask(__name__)
-
-#we need random key for SECRET_KEY (Recaptcha)
-SECRET_KEY = os.urandom(32)
 
 #reCaptcha Google config
 app.config['RECAPTCHA_PUBLIC_KEY']= '6LdWmOIUAAAAALBnW-byjnxFgGTN4otXh8dAc4sI'
 app.config['RECAPTCHA_PRIVATE_KEY']='6LdWmOIUAAAAAE2HuImf23hescbgdftadEv0T60P'
-app.config['SECRET_KEY'] = SECRET_KEY
-
+app.config['SECRET_KEY'] = 'DhI1U6g9Pv8wRCGakI3QTAlpWbG4'
+app.config['TESTING'] = True
 
 @app.route('/')
 def index():
@@ -28,12 +24,12 @@ def contact():
     return render_template('contact.html')
 
 class SingUpForm(Form):
-    name = StringField('Name', validators=[validators.InputRequired(), validators.Length(min=2, max=50)])
-    email = StringField('Email', validators=[validators.InputRequired(), validators.Length(min=5,max=70), validators.Email()])
+    name = StringField('Name', validators=[validators.InputRequired(), validators.Length(min=2, max=50, message='Name must be between 2 and 50 characters long')])
+    email = StringField('Email', validators=[validators.InputRequired(), validators.Length(min=5,max=70, message='Email must be between 2 and 50 characters long'), validators.Email()])
     password = PasswordField('Password', [
         validators.InputRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match'),
-        validators.Length(min=6,max=50)
+        validators.EqualTo('confirm', message='password does not match'),
+        validators.Length(min=6,max=50,message='Password must be between 6 and 50 characters long')
     ])
     confirm = PasswordField('Confirm Password',[validators.InputRequired()])
     recaptcha = RecaptchaField(validators=[Recaptcha(message="Please Check the reCAPTCHA box")])
@@ -51,7 +47,7 @@ class Login(Form):
     email = StringField('Email', validators=[validators.InputRequired(), validators.Length(min=5,max=70), validators.Email()])
     password = PasswordField('Password', [
         validators.InputRequired(),
-        validators.Length(min=6,max=50)
+        validators.Length(min=6,max=50,message='Password must be between 6 and 50 characters long')
     ])
     recaptcha = RecaptchaField(validators=[Recaptcha(message="Please Check the reCAPTCHA box")])
 
