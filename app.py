ca892a8d3a7e43aa5a5922bc3f0826f233cc157f
Waleed BinSaad <wbinsaad@stu.kau.edu.sa>
2020-03-23 06:13:32 +0300

    adding validate_on_submit

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, flash, url_for, session, logging, request, redirect
-from wtforms import Form, StringField, PasswordField, validators
-from flask_wtf import RecaptchaField, Recaptcha
+from wtforms import Form, StringField, PasswordField, BooleanField, validators
+from flask_wtf import RecaptchaField, Recaptcha, FlaskForm
 from passlib.hash import sha256_crypt
 from datetime import timedelta
+from flask_login import LoginManager
 from functools import wraps
 
 
@@ -15,7 +16,7 @@ app.config['SECRET_KEY'] = 'DhI1U6g9Pv8wRCGakI3QTAlpWbG4'
 app.config['TESTING'] = True
 
 #configer timeout for the session
-app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(seconds=30)
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(seconds=10)
 
 app.config['MYSQL_HOST'] = '104.198.153.176'
 app.config['MYSQL_USER'] = 'root'
@@ -67,18 +68,22 @@ def SingUp():
     
     return render_template('singup.html', form=form)
 
-class Login(Form):
+class Login(FlaskForm):
     email = StringField('Email', validators=[validators.InputRequired(), validators.Length(min=5,max=70), validators.Email()])
     password = PasswordField('Password', [
         validators.InputRequired(),
         validators.Length(min=6,max=50,message='Password must be between 6 and 50 characters long')
     ])
+    rememberMe = BooleanField('Remember me')
     recaptcha = RecaptchaField(validators=[Recaptcha(message="Please Check the reCAPTCHA box")])
 
 @app.route('/login', methods=['get','post'])
 def login():
     form = Login(request.form)
-    if request.method == 'POST' and form.validate():
+    if form.validate_on_submit():
+        if form.rememberMe.data == True:
+            session['_permanent'] = True
+
         session['email'] = form.email.data
         session['password'] = form.password.data
         return redirect(url_for('dashboard'))
